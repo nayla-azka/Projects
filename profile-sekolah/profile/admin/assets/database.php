2fe96f4344b47a8ef9d1fa5022416556e6d3afbd
Nayla Azka <naylaazkafs@gmail.com>
2025-03-03 20:16:29 +0700

    Add files via upload

---
+++ profile-sekolah/profile/admin/assets/database.php
@@ -0,0 +1,17 @@
+<?php  
+class database{ 
+  
+	public $host = "localhost";
+	public $username = "root";
+	public $password = "";
+	public $database = "db_profil";
+	public $koneksi = "";
+
+function __construct(){
+		$this->koneksi = mysqli_connect($this->host, $this->username, $this->password,$this->database);
+		if (mysqli_connect_errno()){
+			echo "Koneksi database gagal : " . mysqli_connect_error(); 
+		}
+	}  
+}
+?>
